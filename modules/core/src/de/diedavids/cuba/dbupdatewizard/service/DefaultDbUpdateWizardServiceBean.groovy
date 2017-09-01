19a54255393502ccef4a9fd43f28ce50040329df
Mario David <mariodavid@arcor.de>
2017-09-01 14:24:29 +0200

    switched default service to groovy

---
+++ modules/core/src/de/diedavids/cuba/dbupdatewizard/service/DefaultDbUpdateWizardServiceBean.groovy
@@ -0,0 +1,29 @@
+package de.diedavids.cuba.dbupdatewizard.service
+
+import de.diedavids.cuba.dbupdatewizard.wizard.Message
+import org.springframework.stereotype.Service
+
+@Service(DbUpdateWizardService.NAME)
+class DefaultDbUpdateWizardServiceBean implements DbUpdateWizardService {
+
+
+    @Override
+    boolean isUpdateAvaliable() {
+        false
+    }
+
+    @Override
+    String getDbUpdateWizardStartMessage() {
+        null
+    }
+
+    @Override
+    Collection<Message> validatePreconditions() {
+        null
+    }
+
+    @Override
+    Collection<Message> executeUpdate() {
+        null
+    }
+}
